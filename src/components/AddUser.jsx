5265665d066e7cebd9af956f3a7e65960ef26e21
thenameisahalya <ahalyavl24@gmail.com>
2024-06-08 10:37:59 +0530

    project done

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -29,7 +29,7 @@ export const AddUser = () => {
                 <label htmlFor="" className="form-label">Website</label>
                 <input type="text" className="form-control" />
                 </div>
-                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <button className="btn btn-success">Add User</button>
                 </div>
              </div>
